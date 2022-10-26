4f899781cbba159ff7b5ed05f09e4f72c4fc4201
Mustafa Ahmed <m.awes780@gmail.com>
2022-10-26 16:46:00 -0500

    minor changes

---
+++ script.js
@@ -0,0 +1,6 @@
+const todayDate = new Date()
+alert (todayDate)
+const numberOne = prompt ("Enter a number")
+const numberTwo = prompt ("Enter another number")
+console.log("Math.min(numberOne, numberTwo) : ",+Math.min(numberOne, numberTwo)) 
+alert (console.log)
