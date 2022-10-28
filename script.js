59630d3b49d272e16e19bd41f62469c7af2b74c4
Mustafa Ahmed <m.awes780@gmail.com>
2022-10-27 19:04:54 -0500

    changes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const todayDate = new Date()
-alert (todayDate)
-const numberOne = prompt ("Enter a number")
-const numberTwo = prompt ("Enter another number")
-console.log("Math.min(numberOne, numberTwo) : ",+Math.min(numberOne, numberTwo)) 
-alert (Math.min(numberOne, numberTwo))
+// day2
+// const todayDate = new Date()
+// alert (todayDate)
+// const numberOne = prompt ("Enter a number")
+// const numberTwo = prompt ("Enter another number")
+// console.log("Math.min(numberOne, numberTwo) : ",+Math.min(numberOne, numberTwo)) 
+// alert (Math.min(numberOne, numberTwo))
+
+// day3
+ 
