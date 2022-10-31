e97ff364cdccd9580510d97c7588102460197298
Mustafa Ahmed <m.awes780@gmail.com>
2022-10-30 20:57:58 -0500

    add homework

--- script.js
+++ script.js
@@ -7,4 +7,11 @@
 // alert (Math.min(numberOne, numberTwo))
 
 // day3
- 
+//  challenge 1
+const firstFavColor = prompt (enter fav color)
+const secondFavColor = prompt (enter second fav color)
+const thirdFavColor = prompt (enter third fav color)
+if (firstFavColor = blue) {
+}
+else if (secondFavColor = grey)
+or else (`better luck next time`)
